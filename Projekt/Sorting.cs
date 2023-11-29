55be42ad78404a7f45ca3f386c0861fe3881ad54
24307 <24307@303-15>
2023-11-29 12:16:27 +0100

    Add project files.

---
+++ Projekt/Sorting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    internal class Sorting
+    {
+        public static void BubbleSort(int[] array)
+        {
+
+        }
+    }
+}
